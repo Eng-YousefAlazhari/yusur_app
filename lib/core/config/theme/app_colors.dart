88a88552e9334ae10db9447c6ad88a637f8e7686
Eng-YousefAlazhari <aforsite79@gmail.com>
2026-05-08 19:05:15 +0300

    add app color

---
+++ lib/core/config/theme/app_colors.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  // primary
+  static const primary200 = Color(0xffAEB6FB);
+  static const primary300 = Color(0xff8895F9);
+  static const primary400 = Color(0xff7181F8);
+  static const primary500 = Color(0xff4C68D5);
+  static const primary600 = Color(0xff4758E0);
+  static const primary700 = Color(0xff3745AF);
+
+  // Text color
+  static const textPrimary = Color(0xff0d0d12);
+  static const textSecondary = Color(0xff334155);
+  static const textTernary = Color(0xff475569);
+  static const textTernaryLite = Color(0xff64748B);
+  //surface
+  static const background = Color(0xffF2F2F2);
+  static const onSurface = Color(0xff9EA2AE);
+  static const container = Color(0xffE5E7EA);
+
+  //wihte color
+  static const white = Color(0xffffffff);
+  static const white80 = Color(0xffD9D9D9);
+
+  //error
+  static const error300 = Color(0xff710e21);
+  static const error200 = Color(0xff96132c);
+  static const error100 = Color(0xffdf1c41);
+  static const error50 = Color(0xffed8296);
+  static const error25 = Color(0xfffadbe1);
+  static const error0 = Color(0xfffff0f3);
+
+  //warning
+  static const warning300 = Color(0xff5c3d1f);
+  static const warning200 = Color(0xff966422);
+  static const warning100 = Color(0xffffbe4c);
+  static const warning50 = Color(0xfffcda83);
+  static const warning25 = Color(0xfffaedcc);
+  static const warning0 = Color(0xfffff6e0);
+
+  // success
+  static const success300 = Color(0xff184e44);
+  static const success200 = Color(0xff28806f);
+  static const success100 = Color(0xff40c4aa);
+  static const success50 = Color(0xff9ee1d4);
+  static const success25 = Color(0xffddf3ef);
+  static const success0 = Color(0xffeffefa);
+}
