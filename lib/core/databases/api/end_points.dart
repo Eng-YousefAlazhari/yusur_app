5b5f0d680164ab498b0c878ffc1716d52ab906bd
Eng-YousefAlazhari <aforsite79@gmail.com>
2026-05-16 11:24:29 +0300

    add EndPoints and ApiKey classes

---
+++ lib/core/databases/api/end_points.dart
@@ -0,0 +1,16 @@
+class EndPoints {
+  static const String baserUrl = "http://127.0.0.1:8000/api/v1/";
+  static const String auth = "auth/";
+  static const String register = "register/";
+  static const String login = "login/";
+  static const String logout = "logout/";
+}
+
+class ApiKey {
+  static String id = "id";
+  static String name = "name";
+  static String username = "username";
+  static String email = "email";
+  static String address = "address";
+  static String city = "city";
+}
