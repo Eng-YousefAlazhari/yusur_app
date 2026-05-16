6475e4e23b19082845d85709df517804f086ae55
Eng-YousefAlazhari <aforsite79@gmail.com>
2026-05-16 11:43:42 +0300

    refactor main.dart to use MaterialApp.router for improved navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:yusur_app/core/config/theme/app_text_styles.dart';
+import 'package:yusur_app/core/routes/app_routes.dart';
 
 void main() {
   runApp(DevicePreview(enabled: !kReleaseMode, builder: (context) => MyApp()));
@@ -13,22 +13,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'تطبيق  لمعرفة دبلومات ودورات المعاهد ',
-              style: AppTextStyles.medium20,
-            ),
-          ],
-        ),
-      ),
+      routerConfig: AppRouter.router,
     );
   }
 }
